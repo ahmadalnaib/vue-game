cfffcc76d3af7f23ff9d703f3c332ab22db7b3a4
ahmedhabeeb73 <alnaib888@gmail.com>
2020-12-10 12:52:57 +0100

    add block

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,30 @@
 <template>
  <h1>Dundung Reaction Timer</h1>
+ <button @click="start" :disabled="isPlaying">Play</button>
+ <block v-if="isPlaying" :delay="delay"/>
 </template>
 
 <script>
-
+import Block from './components/Block.vue'
 
 export default {
   name: 'App',
   components: {
-   
+   Block
+  },
+  data(){
+    return{
+
+   isPlaying:false,
+   delay:null,
+
+    }
+  },
+  methods:{
+     start(){
+      this.delay=2000+Math.random() * 5000
+      this.isPlaying=true
+     }
   }
 }
 </script>
